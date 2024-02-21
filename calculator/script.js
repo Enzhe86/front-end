02cb2d0bf0e5a8e56b93a0d4ac89841da8cebe51
Enzhe86 <enzhe.shamsutdinova@gmail.com>
2024-02-21 08:24:53 +0300

    feat: validate input string

--- calculator/script.js
+++ calculator/script.js
@@ -1,13 +1,20 @@
 function show(value) {
-    document.getElementById('display').value += value;
+  document.getElementById('display').value += value;
 }
 
 function calculate() {
-    let displayValue = document.getElementById('display').value;
+  let displayValue = document.getElementById('display').value;
+  if (displayValue.match(/[\+\-\*\/]{2,}/)) {
+    alert('The expression is incorrect');
+    clearScr();
+  } else {
     let result = eval(displayValue);
-    return document.getElementById('display').value = result;
+    document.getElementById('display').value = result;
+  }
 }
 
 function clearScr() {
-    document.getElementById('display').value = '';
+  document.getElementById('display').value = '';
 }
+
+
